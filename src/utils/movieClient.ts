2f0c9b67d5736cf3b9728b0a0a97c13eb3a48668
sand Ratiaray <sratiaray@gmail.com>
2024-08-09 16:44:59 +0200

    add searching functionnality

--- src/utils/movieClient.ts
+++ src/utils/movieClient.ts
@@ -1,11 +1,23 @@
 // Library who send error if we import it in a client component
 import 'server-only';
 
-export const getMovieByPath = (path: string, language: string = 'fr-Fr') => {
+interface IParams {
+  key: string;
+  value: string;
+}
+
+export const getMovieByPath = (
+  path: string,
+  params: IParams[] = [],
+  language: string = 'fr-Fr'
+) => {
   const url = new URL(`${process.env.TMDB_API_URL}${path}`);
 
   url.searchParams.append('api_key', process.env.TMDB_API_KEY!);
   url.searchParams.append('language', language);
+  params.forEach((param) => {
+    url.searchParams.append(param.key, param.value);
+  });
 
   return fetch(url).then((res) => res.json());
 };
