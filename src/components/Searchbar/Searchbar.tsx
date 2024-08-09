dde432935b4e89523226766129ac40b6b7a2baf0
sand Ratiaray <sratiaray@gmail.com>
2024-08-09 14:03:04 +0200

    add search bar component

---
+++ src/components/Searchbar/Searchbar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Searchbar = () => {
+    return (
+        <div>
+            <input type="text" placeholder='Titres,personnes,genres' />
+        </div>
+    )
+}
+
+export default Searchbar;
