c33080763ca79c528bc5f69303d9ebc8a28ca464
sand Ratiaray <sratiaray@gmail.com>
2024-09-06 15:58:05 +0200

    add middleware i18n

---
+++ src/utils/i18n.ts
@@ -0,0 +1,32 @@
+import { match } from '@formatjs/intl-localematcher';
+import Negotiator from 'negotiator';
+import { NextRequest } from 'next/server';
+
+// prefix system
+export const availableLocales = ['en', 'fr'];
+export const defaultLocale = 'fr';
+
+// check headers and filter language by preference
+export const getPrefferedLocale = (request: NextRequest) => {
+  const headers = {
+    'accept-language': request.headers.get('accept-language')!,
+  };
+  const languages = new Negotiator({ headers }).languages();
+
+  // return locale by preference , availables and by default
+  return match(languages, availableLocales, defaultLocale);
+};
+
+// check if the locale exist and return the right URL if not
+export const getLocaleUrlToRedirect = (request: NextRequest) => {
+  const pathname = request.nextUrl.pathname;
+  const pathnameIsMissingLocale = availableLocales.every(
+    (locale) => !pathname.startsWith(`/${locale}`) && pathname !== `/${locale}`
+  );
+
+  if (pathnameIsMissingLocale) {
+    const locale = getPrefferedLocale(request);
+
+    return new URL(`/${locale}/${pathname}`, request.url);
+  }
+};
