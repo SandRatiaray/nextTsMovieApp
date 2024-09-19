734b1b8896c6a4d23ab7bebba2c3570e337e4dc8
sand Ratiaray <sratiaray@gmail.com>
2024-09-19 16:20:06 +0200

    add profile page

---
+++ src/components/logout-button/LogoutButton.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+
+const LogoutButton = () => {
+    return (
+        <div>
+            <button onClick={() => signOut({ callbackUrl: "/" })}>Déconnexion</button>
+        </div >
+    );
+};
+
+export default LogoutButton;
