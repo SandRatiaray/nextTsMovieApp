c0e683639c4a33349b6d5ab5bfefd07aa25bdb0e
sand Ratiaray <sratiaray@gmail.com>
2024-09-26 11:21:55 +0200

    fix delete empty poster

--- src/components/similar-movies/SimilarMovies.tsx
+++ src/components/similar-movies/SimilarMovies.tsx
@@ -1,4 +1,4 @@
-import { IMovie, IMovieSimilar } from '@/interfaces/movie';
+import { IMovie } from '@/interfaces/movie';
 import { getMovieByPath } from '@/utils/movieClient';
 import styles from './SimilarMovies.module.scss'
 import React from 'react';
@@ -10,7 +10,7 @@ const SimilarMovies = async ({ movieId, locale }: { movieId: string, locale: Loc
     return (
         <div className={styles.similar}>
             <div className={styles.list}>
-                {results.slice(0, 6).map((movie: IMovie) => (
+                {results.filter((movie: IMovie) => movie.poster_path).slice(0, 6).map((movie: IMovie) => (
                     <Card media={movie} key={movie.id} locale={locale} />
                 ))}
             </div>
